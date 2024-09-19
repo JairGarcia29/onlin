7709ae00b6d7daa249ebac634789f1be57255622
FabianElFail <fabianecasasolivos@hotmail.com>
2024-09-19 08:21:36 -0500

    Primer commit

---
+++ P2/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n, v=0,a=0,r = 0;
+        
+        n = 1;
+
+        while (n >= 0& n<=9) {
+            Console.WriteLine("Ingrese su ultimo número de placa");
+            n = int.Parse(Console.ReadLine());
+            if (n >= 0 & n <= 3) {
+                v++;
+            } else if (n >= 4 & n <= 6) {
+                a++;
+            } else if (n >= 7 & n <= 9)
+            {
+                r++;
+            }
+        }
+        Console.WriteLine("PROGRAMA TERMINADO");
+        Console.WriteLine($"Verde: {v}");
+        Console.WriteLine($"Azul: {a}");
+        Console.WriteLine($"Rojo: {r}");
+
+
+    }
+}
